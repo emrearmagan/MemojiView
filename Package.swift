7a31a06ab248010b711b4e12fcb5f05e92b877c4
Emre Armagan <emrearmagan.dev@gmail.com>
2025-01-01 12:39:01 +0100

    add SPM support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MemojiView",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "MemojiView", targets: ["MemojiView"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "MemojiView",
+            dependencies: [],
+            path: "MemojiView"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
